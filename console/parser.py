c69beb0a70a05b0a525715f2e13d75d2a83c48a3
Aleksandr Novikov <pwsdotru@gmail.com>
2026-02-26 23:23:24 +0300

    Начало парсера

---
+++ console/parser.py
@@ -0,0 +1,45 @@
+from zipfile import ZipFile
+from xml.etree import ElementTree as ET
+import re
+
+ns = {
+        "office": "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
+        "text": "urn:oasis:names:tc:opendocument:xmlns:text:1.0",
+        "table": "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
+    }
+geo_data = {}
+region_count = -1
+data_count = 0
+
+regexp = re.compile("[0-9]{1,3}\\.")
+
+path = "doc.odt"
+with ZipFile(path) as z, z.open("content.xml") as f:
+    root = ET.parse(f).getroot()
+
+
+for cell in root.findall(f".//table:table-cell", ns):
+    headers = cell.findall(f".//text:h", ns)
+    if len(headers) > 0:
+        region = "";
+        for h in headers:
+            if None != h.text:
+                region = region + h.text
+        region_count = region_count + 1
+        geo_data[region_count] = {"region": region, "data": {}}
+        data_count = 0
+
+    paragraphs = cell.findall(f".//text:p", ns)
+    if region_count >= 0 and len(paragraphs) > 2:
+        geo_data[region_count]["data"][data_count] = {"title": "", "points": []}
+        for p in paragraphs:
+            if None != p.text:
+                if regexp.match(p.text):
+                    geo_data[region_count]["data"][data_count]["points"].append(p.text)
+                else:
+                    if geo_data[region_count]["data"][data_count]["title"] != "":
+                        geo_data[region_count]["data"][data_count]["title"] += " "
+                    geo_data[region_count]["data"][data_count]["title"] = geo_data[region_count]["data"][data_count]["title"] + p.text
+        data_count = data_count + 1
+
+print(geo_data)
