e1c50410962401ef1fd456bf9af06b639c28a44d
Aleksandr Novikov <pwsdotru@gmail.com>
2026-03-25 21:02:20 +0300

    Сохранение результата работы в JSON для дальнейшей обработки

--- console/parser.py
+++ console/parser.py
@@ -5,9 +5,13 @@ import os
 import sys
 import json
 
+#словарь для сохранения статистики
 stats ={"total": 0, "error": 0, "error_parse": [], "error_convert": [], "empty_points": []}
 
-#1. 55°43'00,7" с.ш. 54°16'41,0" в.д.
+#
+#Парсиг строки вида #1. 55°43'00,7" с.ш. 54°16'41,0" в.д.
+#и извлечение из нее широты и долготы
+#
 def parse_coords(input):
     res = {"lng": "0.0", "lat": "0.0"}
     stats["total"] += 1
@@ -23,6 +27,9 @@ def parse_coords(input):
     stats["error_parse"].append(input)
     return None 
 
+#
+#Парсинг и конвертация координат вида 55°43'00,7" с.ш. в десятичные координаты
+#
 def convert_coords(input):
     params = {
         "degrees":{"value": 0, "separator": '°', "isint": True},
@@ -68,7 +75,7 @@ def convert_coords(input):
     res = params["degrees"]["value"] + params["minutes"]["value"]/60.0 + params["seconds"]["value"]/3600.0
     return res*sign
 #
-#Получение параметров коммандной строки
+#Получение параметров командной строки
 #
 def parse_arg():
     params = {
@@ -76,24 +83,45 @@ def parse_arg():
         'output': 'geojson',
         'pretty': 1
     }
-    for i in range(1, len(sys.argv)):
+    op_len = len(sys.argv)
+    for i in range(1, op_len):
         arg = sys.argv[i].strip()
-        if arg != '' and os.path.exists(arg):
-            params['input'] = arg
+        if arg[0:2] == '--':
+            key = arg[2:]
+            #если есть параметр с таким именем, то пытаемся присвоить ему значение
+            if key in params:
+                if i+1 < op_len:
+                    value = sys.argv[i+1]
+                    #если следующим идет другое ключевое поле, то значение пустое
+                    if value[0:2] == '--':
+                        params[key] = ''
+                    else:
+                        params[key] = value.strip()
+                    #следующего за ключевым словом параметра нет
+                else:
+                    params[key] =  ''
+        else:
+            if arg != '' and os.path.exists(arg):
+                params['input'] = arg
+
     return params
 
 #
 #Основной код
 #
+
+#namespace для XML парсера
 ns = {
         "office": "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
         "text": "urn:oasis:names:tc:opendocument:xmlns:text:1.0",
         "table": "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
     }
+
 geo_data = {}
 region_count = -1
 data_count = 0
 
+#регулярное выражение, для того чтобы определить наличие координат в строке текста
 regexp = re.compile("[0-9]{1,3}\\.")
 
 script_params = parse_arg()
@@ -119,9 +147,12 @@ for cell in root.findall(f".//table:table-cell", ns):
 
     paragraphs = cell.findall(f".//text:p", ns)
     if region_count >= 0 and len(paragraphs) > 2:
-        geo_data[region_count]["data"][data_count] = {"title": "", "points": []}
+        geo_data[region_count]["data"][data_count] = {"title": "", "points": [], "original": {}}
+        line_count = 0
         for p in paragraphs:
             if None != p.text:
+                geo_data[region_count]["data"][data_count]["original"][line_count] = p.text
+                line_count += 1
                 if regexp.match(p.text):
                     point = parse_coords(p.text)
                     if point != None:
@@ -134,6 +165,9 @@ for cell in root.findall(f".//table:table-cell", ns):
                     geo_data[region_count]["data"][data_count]["title"] = geo_data[region_count]["data"][data_count]["title"] + p.text
         data_count = data_count + 1
 
+if script_params["output"] == "raw":
+    print(json.dumps(geo_data, indent=4))
+    exit()
 
 output_json = {"type":"FeatureCollection", "features":[]}
 for rid, region in geo_data.items():
